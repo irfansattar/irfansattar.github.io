f0792241fd51f462cc802f8b453bbacf2189b3c5
Irfan Sattar <23389884+irfansattar@users.noreply.github.com>
2020-03-22 19:03:59 -0700

    Updates

---
+++ precache-manifest.05919b8f153b2b9888fcff29c7964017.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54ac207c578cb5ad173548359fbec2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6073d21e3a3cfb6e89c8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "712fb36af5daf9ba2c23",
+    "url": "/static/js/2.3cdcaa80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3cdcaa80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6073d21e3a3cfb6e89c8",
+    "url": "/static/js/main.4b019f74.chunk.js"
+  },
+  {
+    "revision": "11ad1388d5af3a6af453",
+    "url": "/static/js/runtime-main.dc2e980f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
